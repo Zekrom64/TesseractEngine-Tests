6aa19644849a86cd3209af37f2837719435d0122
Zekrom64 <bfgmusicscores@gmail.com>
2023-02-19 15:43:56 -0600

    Small DirectX update

--- TesseractEngine-Tests-Win32/Program.cs
+++ TesseractEngine-Tests-Win32/Program.cs
@@ -21,17 +21,25 @@ namespace Tesseract.Tests {
 			var adapterFactory = DXCore.CreateAdapterFactory<IDXCoreAdapterFactory>();
 			var adapterAttribs = sp.Values(DXCore.AttributeD3D12Graphics);
 			var adapterList = COMHelpers.GetObjectFromCOMGetter<IDXCoreAdapterList>((in Guid riid) => adapterFactory.CreateAdapterList((uint)adapterAttribs.ArraySize, adapterAttribs.Ptr, riid))!;
-			Console.WriteLine("[DXCore Raw] # of adapters: " + adapterList.GetAdapterCount());
+			Console.WriteLine($"[DXCore Raw] # of adapters: {adapterList.GetAdapterCount()}");
+
+			Span<bool> pHwd = stackalloc bool[1];
 
 			uint nadapters = adapterList.GetAdapterCount();
 			for (uint i = 0; i < nadapters; i++) {
-				Console.WriteLine("[DXCore Raw] Adapter #" + i);
+				Console.WriteLine($"[DXCore Raw] Adapter #{i}");
 				var adapter = COMHelpers.GetObjectFromCOMGetter<IDXCoreAdapter>((in Guid riid) => adapterList.GetAdapter(i, riid))!;
 				if (adapter.IsPropertySupported(DXCoreAdapterProperty.DriverDescription)) {
 					nuint szDesc = adapter.GetPropertySize(DXCoreAdapterProperty.DriverDescription);
-					ManagedPointer<byte> pDesc = new((int)szDesc);
+					using ManagedPointer<byte> pDesc = new((int)szDesc);
 					adapter.GetProperty(DXCoreAdapterProperty.DriverDescription, (uint)pDesc.ArraySize, pDesc.Ptr);
-					Console.WriteLine("[DXCore Raw]     DriverDescription=" + MemoryUtil.GetUTF8(pDesc.Span));
+					Console.WriteLine($"[DXCore Raw]     DriverDescription={MemoryUtil.GetUTF8(pDesc.Span)}");
+				}
+				if (adapter.IsPropertySupported(DXCoreAdapterProperty.IsHardware)) {
+					Console.WriteLine($"[DXCore Raw]     IsHardware={adapter.GetProperty<bool>(DXCoreAdapterProperty.IsHardware)}");
+				}
+				if (adapter.IsPropertySupported(DXCoreAdapterProperty.DedicatedAdapterMemory)) {
+					Console.WriteLine($"[DXCore Raw]     DedicatedAdapterMemory={adapter.GetProperty<ulong>(DXCoreAdapterProperty.DedicatedAdapterMemory)}");
 				}
 				adapter.Release();
 			}
